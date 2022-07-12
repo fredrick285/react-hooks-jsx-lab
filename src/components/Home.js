9cb9efc8fc86b7452c5649e6047cb7ca64873c4e
Fredrick <fredrick.lokidor@student.moringaschool.com>
2022-07-13 02:30:57 +0300

    complete commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,8 +2,13 @@ import React from "react";
 import { name, city } from "../data/data.js";
 
 function Home() {
+  const myStyle={color:"firebrick"}
   // update the JSX being returned!
-  return <div>Home</div>;
+  return <div id="home">
+    <h1 style={ myStyle}> {name} is a Web Developer from  {city}</h1>
+   
+   
+  </div>
 }
 
 export default Home;
